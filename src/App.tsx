ff84ed9077ac6b44ad1816faac5e801e8f7cc285
TheDevMinerTV <tobigames200@gmail.com>
2022-11-02 01:14:06 +0100

    fix: input selectable

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { MdSearch } from "react-icons/md";
 function App() {
   return (
     <div className="yeet">
-      <div className="flex items-center">
-        <MdSearch className="text-2xl text-icon" />
+      <div className="flex items-center select-none">
+        <MdSearch className="text-2xl text-icon select-none" />
 
         <input
           type="text"
           placeholder="Godray Search"
-          className="ml-2 bg-transparent text-xl w-full text-input"
+          className="ml-2 bg-transparent text-xl w-full text-input select-text"
           autoFocus
         />
       </div>
